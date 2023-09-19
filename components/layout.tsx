2bf764d00bd45f7251f1c0c09ac869a350dfae68
Peter Sideris <petersid2022@gmail.com>
2023-09-19 16:22:42 +0300

    add: added strangelove link

--- components/layout.tsx
+++ components/layout.tsx
@@ -30,12 +30,14 @@ export default function Layout({
                     name="description"
                     content="petrside"
                 />
-                <meta
-                    property="og:image"
-                    content={`https://og-image.vercel.app/${encodeURI(
-                        siteTitle,
-                    )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-                />
+                {/*
+                    <meta
+                        property="og:image"
+                        content={`https://og-image.vercel.app/${encodeURI(
+                            siteTitle,
+                        )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+                    />
+                */}
                 <meta name="og:title" content={siteTitle} />
             </Head>
             <header className={styles.header}>
