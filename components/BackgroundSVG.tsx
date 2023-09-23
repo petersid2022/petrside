1a74cd73ac01f46100d745cb7da27b7f03598f0c
Peter Sideris <petersid2022@gmail.com>
2023-09-23 22:39:27 +0300

    test: website frontend redesign

--- components/BackgroundSVG.tsx
+++ components/BackgroundSVG.tsx
@@ -1,163 +1,168 @@
 import * as THREE from 'three';
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import utilStyles from '../styles/utils.module.css';
 
 const BackgroundSVG: React.FC = () => {
+    const containerRef = useRef<HTMLDivElement>(null);
+
     useEffect(() => {
-        const canvas = document.getElementById('webgl-canvas') as HTMLCanvasElement;
+        let container: HTMLDivElement;
+        let camera: THREE.PerspectiveCamera;
+        let scene: THREE.Scene;
+        let raycaster: THREE.Raycaster;
+        let renderer: THREE.WebGLRenderer;
+        let parentTransform: THREE.Object3D;
+        let sphereInter: THREE.Mesh;
+        let pointer = new THREE.Vector2();
+        let radius = 150;
+        let theta = 0;
 
-        // Create a scene, camera, and renderer
-        const scene = new THREE.Scene();
+        const init = () => {
+            container = containerRef.current!;
+            document.body.appendChild(container);
 
-        // Define the dimensions for the OrthographicCamera
-        const left = -1;
-        const right = 1;
-        const top = 1;
-        const bottom = -1;
-        const near = 0;
-        const far = -1;
+            camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 1, 10000);
 
-        // Calculate the aspect ratio based on window dimensions
-        const aspect = window.innerWidth / window.innerHeight;
+            scene = new THREE.Scene();
+            scene.background = new THREE.Color(0xf0f0f0);
 
-        const camera = new THREE.OrthographicCamera(
-            left * aspect,
-            right * aspect,
-            top,
-            bottom,
-            near,
-            far
-        );
+            const geometry = new THREE.SphereGeometry(5);
+            const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 
-        const renderer = new THREE.WebGLRenderer({ canvas });
+            sphereInter = new THREE.Mesh(geometry, material);
+            sphereInter.visible = false;
+            scene.add(sphereInter);
 
-        renderer.setPixelRatio(window.devicePixelRatio);
-        renderer.setClearColor(0x666666);
-        renderer.setSize(window.innerWidth, window.innerHeight);
+            const lineGeometry = new THREE.BufferGeometry();
+            const points: number[] = [];
 
-        // Create a mesh
-        const geometry = new THREE.BufferGeometry();
-        const position = new Float32Array([
-            -1.0, -1.0, 0.0,
-            1.0, -1.0, 0.0,
-            -1.0, 1.0, 0.0,
-            1.0, -1.0, 0.0,
-            -1.0, 1.0, 0.0,
-            1.0, 1.0, 0.0,
-        ]);
-        geometry.setAttribute('position', new THREE.BufferAttribute(position, 3));
+            const point = new THREE.Vector3();
+            const direction = new THREE.Vector3();
 
-        const vertexShader = `
-      attribute vec3 position;
-      void main() {
-        gl_Position = vec4(position, 1.0);
-      }
-    `;
+            for (let i = 0; i < 100; i++) {
+                direction.x += Math.random() - 0.5;
+                direction.y += Math.random() - 0.5;
+                direction.z += Math.random() - 0.5;
+                direction.normalize().multiplyScalar(10);
 
-        const fragmentShader = `
-      precision highp float;
-      uniform vec2 resolution;
-      uniform float time;
-      uniform float xScale;
-      uniform float yScale;
-      uniform float distortion;
+                point.add(direction);
+                points.push(point.x, point.y, point.z);
+            }
 
-      void main() {
-        vec2 p = (gl_FragCoord.xy * 2.0 - resolution) / min(resolution.x, resolution.y);
-        float d = length(p) * distortion;
+            lineGeometry.setAttribute('position', new THREE.Float32BufferAttribute(points, 3));
 
-        float rx = p.x * (1.0 + d);
-        float gx = p.x;
-        float bx = p.x * (1.0 - d);
+            parentTransform = new THREE.Object3D();
+            parentTransform.position.x = Math.random() * 40 - 20;
+            parentTransform.position.y = Math.random() * 40 - 20;
+            parentTransform.position.z = Math.random() * 40 - 20;
 
-        float r = 0.05 / abs(p.y + sin((rx + time) * xScale) * yScale);
-        float g = 0.05 / abs(p.y + sin((gx + time) * xScale) * yScale);
-        float b = 0.05 / abs(p.y + sin((bx + time) * xScale) * yScale);
+            parentTransform.rotation.x = Math.random() * 2 * Math.PI;
+            parentTransform.rotation.y = Math.random() * 2 * Math.PI;
+            parentTransform.rotation.z = Math.random() * 2 * Math.PI;
 
-        gl_FragColor = vec4(r, g, b, 1.0);
-      }
-    `;
+            parentTransform.scale.x = Math.random() + 0.5;
+            parentTransform.scale.y = Math.random() + 0.5;
+            parentTransform.scale.z = Math.random() + 0.5;
 
-        const uniforms = {
-            resolution: { type: 'v2', value: new THREE.Vector2(window.innerWidth, window.innerHeight) },
-            time: { type: 'f', value: 0.0 },
-            //xScale: { type: 'f', value: 1.0 },
-            //yScale: { type: 'f', value: 0.5 },
-            //distortion: { type: 'f', value: 0.05 },
-            xScale: { type: 'f', value: 5.0 },
-            yScale: { type: 'f', value: 0.2 },
-            distortion: { type: 'f', value: 0.1 },
+            for (let i = 0; i < 85; i++) {
+                let object: THREE.Line;
+                const lineMaterial = new THREE.LineBasicMaterial({ color: Math.random() * 0xffffff });
+
+
+                if (Math.random() > 0.5) {
+                    object = new THREE.Line(lineGeometry, lineMaterial);
+                } else {
+                    object = new THREE.LineSegments(lineGeometry, lineMaterial);
+                }
+
+                object.position.x = Math.random() * 400 - 200;
+                object.position.y = Math.random() * 400 - 200;
+                object.position.z = Math.random() * 400 - 200;
+
+                object.rotation.x = Math.random() * 2 * Math.PI;
+                object.rotation.y = Math.random() * 2 * Math.PI;
+                object.rotation.z = Math.random() * 2 * Math.PI;
+
+                object.scale.x = Math.random() + 0.5;
+                object.scale.y = Math.random() + 0.5;
+                object.scale.z = Math.random() + 0.5;
+
+                parentTransform.add(object);
+            }
+
+            scene.add(parentTransform);
+
+            raycaster = new THREE.Raycaster();
+            raycaster.params.Line.threshold = 0;
+
+            renderer = new THREE.WebGLRenderer({ antialias: true });
+            renderer.setPixelRatio(window.devicePixelRatio);
+            renderer.setSize(window.innerWidth, window.innerHeight);
+            container.appendChild(renderer.domElement);
+
+            document.addEventListener('pointermove', onPointerMove);
+
+            window.addEventListener('resize', onWindowResize);
         };
 
-        const material = new THREE.RawShaderMaterial({
-            vertexShader,
-            fragmentShader,
-            uniforms,
-            side: THREE.DoubleSide,
-        });
+        const onWindowResize = () => {
+            camera.aspect = window.innerWidth / window.innerHeight;
+            camera.updateProjectionMatrix();
+            renderer.setSize(window.innerWidth, window.innerHeight);
+        };
 
-        const mesh = new THREE.Mesh(geometry, material);
-        scene.add(mesh);
+        const onPointerMove = (event: MouseEvent) => {
+            pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+            pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
+        };
 
-        // Animation
         const animate = () => {
             requestAnimationFrame(animate);
+            render();
+        };
 
-            // Update shader time
-            //uniforms.time.value += 0.01;
-            uniforms.time.value += 0.005;
+        const render = () => {
+            theta += 0.1;
+
+            camera.position.x = radius * Math.sin(THREE.MathUtils.degToRad(theta));
+            camera.position.y = radius * Math.sin(THREE.MathUtils.degToRad(theta));
+            camera.position.z = radius * Math.cos(THREE.MathUtils.degToRad(theta));
+            camera.lookAt(scene.position);
+
+            camera.updateMatrixWorld();
+
+            // find intersections
+
+            raycaster.setFromCamera(pointer, camera);
+
+            const intersects = raycaster.intersectObjects(parentTransform.children, true);
+
+            if (intersects.length > 0) {
+                sphereInter.visible = true;
+                sphereInter.position.copy(intersects[0].point);
+            } else {
+                sphereInter.visible = false;
+            }
 
             renderer.render(scene, camera);
         };
 
-        // Handle window resize
-        const onResize = () => {
-            const windowWidth = window.innerWidth;
-            const windowHeight = window.innerHeight;
+        init();
+        animate();
 
-            const newAspect = windowWidth / windowHeight;
-
-            // Update the camera's dimensions based on the new aspect ratio
-            camera.left = left * newAspect;
-            camera.right = right * newAspect;
-            camera.top = top;
-            camera.bottom = bottom;
-
-            camera.updateProjectionMatrix();
-
-            renderer.setSize(windowWidth, windowHeight);
-            uniforms.resolution.value.set(windowWidth, windowHeight);
+        // Clean up on unmount
+        return () => {
+            if (containerRef.current) {
+                containerRef.current.removeChild(renderer.domElement);
+            }
+            window.removeEventListener('resize', onWindowResize);
+            document.removeEventListener('pointermove', onPointerMove);
         };
-
-        const isDesktop = window.innerWidth >= 768; // Adjust the breakpoint as needed
-
-        if (isDesktop && typeof window !== 'undefined') {
-            // Only attach event listeners and create dat.gui if in the browser environment
-            window.addEventListener('resize', onResize);
-
-            // Start animation loop
-            animate();
-
-            // Optional: Set up GUI controls using dat.gui
-            const dat = require('dat.gui'); // Import dat.gui here
-            const gui = new dat.GUI();
-            gui.closed = true;
-            gui.add(uniforms.xScale, 'value', 1.0, 5.0).name('X Scale');
-            gui.add(uniforms.yScale, 'value', 0.2, 1.0).name('Y Scale');
-            gui.add(uniforms.distortion, 'value', 0.001, 0.1).name('Distortion');
-        }
-
-        if (!isDesktop){
-            // Only attach event listeners and create dat.gui if in the browser environment
-            window.addEventListener('resize', onResize);
-
-            // Start animation loop
-            animate();
-        }
     }, []);
 
-    return <div className={utilStyles.backsvg}><canvas id="webgl-canvas"></canvas></div>;
+    return (
+        <div className={utilStyles.backsvg} ref={containerRef} />
+    );
 };
 
 export default BackgroundSVG;
-
--- components/layout.tsx
+++ components/layout.tsx
@@ -26,10 +26,8 @@ export default function Layout({
         <div className={styles.container}>
             <Head>
                 <link rel="icon" href="/favicon.ico" />
-                <meta
-                    name="description"
-                    content="petrside"
-                />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+                <meta name="description" content="petrside" />
                 <meta name="og:title" content={siteTitle} />
             </Head>
             <header className={styles.header}>
